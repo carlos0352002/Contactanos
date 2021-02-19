dee612604d753d0b3f222a3ca980553cf252bdd5
carlos0352002 <75769633+carlos0352002@users.noreply.github.com>
2021-02-19 00:31:18 -0400

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+$destino = "carlosoropeza035@gmail.com";
+$nombre = $_POST["nombre"];
+$correo = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . "/nCorreo: " . $correo . "/nMensaje: " . $mensaje;
+mail($destino,"contacto", $contenido);
+header("Location:Gracias.html");
+?>
---
+++ main.js
@@ -0,0 +1,19 @@
+const btnToggle = document.querySelector('.btn-toogle');
+
+
+btnToggle.addEventListener('click', function () {
+document.getElementById('Sidebar').classList.toggle('carlospersonalidas')
+});
+
+
+btnToggle.addEventListener('click', function() {
+  document.getElementById('spnabtn').classList.toggle('btnspnar')
+
+});
+
+
+btnToggle.addEventListener('click', function() {
+  document.getElementById('spnabtn2').classList.toggle('btnspnar2')
+
+
+});
